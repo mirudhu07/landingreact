b1676374283d47658627f88f13c35bcf72bd4d3b
mirudhu07 <mirudhusanjeevini@gmail.com>
2025-02-12 16:04:52 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import About from "./pages/About"
 import Contact from "./pages/Contact"
 
 import { Route, Routes} from "react-router-dom"
-import Navlinks from "./components/Navlinks/Navlinks"
+import Navlinks from "./components/navlinks/Navlinks"
+
 function App() {
   return (
 
